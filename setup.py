39b29aad758b87829d11f23afef08113d3e93a1c
yuloveyet <liyu@mail.nwpu.edu.cn>
2020-11-21 13:45:43 +0800

    version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',  # Required
+    version='0.1.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -160,7 +160,7 @@ setup(
     # },
     entry_points={  # Optional
         'console_scripts': [
-            'simplelayout=simplelayout/__main__:main',
+            'simplelayout=simplelayout:__main__',
         ],
     },
     # List additional URLs that are relevant to your project as a dict.
